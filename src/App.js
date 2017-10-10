e7c38915d986d7075c55868931a9ab3dae8e0a24
FanQueen <alexander.korobka@mail.ru>
2017-10-10 23:52:08 +0300

    add simple box with pure On/Off state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
+import logo from "./logo.svg";
+import "./App.css";
+
+const Button = styled.button`
+  background: lightgray;
+  border: 0;
+  height: 150px;
+  width: 150px;
+  margin: 100px;
+
+  ${props => props.clicked && css`background: #5227ec;`};
+`;
+
+class SimpleBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
+  render() {
+    return (
+      <Button
+        clicked={this.state.clicked}
+        onClick={() =>
+          this.setState(() => {
+            return { clicked: !this.state.clicked };
+          })}
+      />
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <SimpleBox />
       </div>
     );
   }

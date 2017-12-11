d0df2f9f9753379e7fcfc2e89b5b53f7fa3f2177
FanQueen <alexander.korobka@mail.ru>
2017-12-11 21:47:15 +0300

    fix error Web Audio API support

--- src/components/SoundBox/SoundBox.js
+++ src/components/SoundBox/SoundBox.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import ColumnOfButtons from "./ColumnOfButtons";
-import GreenButton from "./ColumnOfButtons/GreenButton";
-import RedButton from "./ColumnOfButtons/RedButton";
-import SandButton from "./ColumnOfButtons/SandButton";
-import VioletButton from "./ColumnOfButtons/VioletButton";
-import styled from "styled-components";
-import WAAClock from "waaclock";
-import audioFiles from "./audioFiles";
+import React, { Component } from 'react';
+import ColumnOfButtons from './ColumnOfButtons';
+import GreenButton from './ColumnOfButtons/GreenButton';
+import RedButton from './ColumnOfButtons/RedButton';
+import SandButton from './ColumnOfButtons/SandButton';
+import VioletButton from './ColumnOfButtons/VioletButton';
+import styled from 'styled-components';
+import WAAClock from 'waaclock';
+import audioFiles from './audioFiles';
 
 export const SoundBoxWrapper = styled.div`
   display: flex;
@@ -15,37 +15,46 @@ export const SoundBoxWrapper = styled.div`
 
 class SoundBox extends Component {
   componentWillMount() {
-    this.soundBank = {};
-    const context =
-      "webkitAudioContext" in window
-        ? new window.webkitAudioContext()
-        : new window.AudioContext();
-
-    this.clock = new WAAClock(context);
-    this.clock.start();
+    const AudioContext =
+      window.AudioContext || window.webkitAudioContext || false;
+    if (AudioContext) {
+      this.soundBank = {};
+      const context = new AudioContext();
+      this.clock = new WAAClock(context);
+      this.clock.start();
+    }
   }
 
   render() {
     const { clock, soundBank } = this;
-    const columnParams = [
-      { audioType: "arp", colorButton: RedButton },
-      { audioType: "bass", colorButton: VioletButton },
-      { audioType: "beat", colorButton: GreenButton },
-      { audioType: "lead", colorButton: SandButton }
-    ];
-    const columnsOfBoxes = columnParams.map(
-      ({ audioType, colorButton }, index) => (
-        <ColumnOfButtons
-          audioFiles={audioFiles}
-          soundBank={soundBank}
-          clock={clock}
-          key={index}
-          audioType={audioType}
-          colorButton={colorButton}
-        />
-      )
+    if (clock) {
+      const columnParams = [
+        { audioType: 'arp', colorButton: RedButton },
+        { audioType: 'bass', colorButton: VioletButton },
+        { audioType: 'beat', colorButton: GreenButton },
+        { audioType: 'lead', colorButton: SandButton }
+      ];
+      const columnsOfBoxes = columnParams.map(
+        ({ audioType, colorButton }, index) => (
+          <ColumnOfButtons
+            audioFiles={audioFiles}
+            soundBank={soundBank}
+            clock={clock}
+            key={index}
+            audioType={audioType}
+            colorButton={colorButton}
+          />
+        )
+      );
+      return <SoundBoxWrapper>{columnsOfBoxes}</SoundBoxWrapper>;
+    }
+    return (
+      <h1>
+        Sorry, but the Web Audio API is not supported by your browser. Please,
+        consider upgrading to the latest version or downloading Google Chrome or
+        Mozilla Firefox
+      </h1>
     );
-    return <SoundBoxWrapper>{columnsOfBoxes}</SoundBoxWrapper>;
   }
 }
 

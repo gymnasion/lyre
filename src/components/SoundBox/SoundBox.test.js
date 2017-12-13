1da5ee5a3521cb68df965955c0894a97207caaab
FanQueen <alexander.korobka@mail.ru>
2017-12-13 21:07:15 +0300

    add Web Audio support test

--- src/components/SoundBox/SoundBox.test.js
+++ src/components/SoundBox/SoundBox.test.js
@@ -5,6 +5,7 @@ import RedButton from './ColumnOfButtons/RedButton';
 import GreenButton from './ColumnOfButtons/GreenButton';
 import SandButton from './ColumnOfButtons/SandButton';
 import VioletButton from './ColumnOfButtons/VioletButton';
+import ErrorMessage from './ErrorMessage';
 
 describe('SoundBox', () => {
   it('renders SoundBox with 4 colored columns', () => {
@@ -13,5 +14,10 @@ describe('SoundBox', () => {
     expect(renderedSoundBox.find(GreenButton)).toHaveLength(4);
     expect(renderedSoundBox.find(SandButton)).toHaveLength(4);
     expect(renderedSoundBox.find(VioletButton)).toHaveLength(4);
+  });
+  it('renders error message if browser does not support Web Audio', () => {
+    window.AudioContext = undefined;
+    const renderedSoundBox = mount(<SoundBox />);
+    expect(renderedSoundBox.find(ErrorMessage)).toHaveLength(1);
   });
 });

1cad52cbfd015975c93357e180a92e2d4e4c6011
FanQueen <alexander.korobka@mail.ru>
2017-12-24 19:03:58 +0300

    refactor getButton

--- src/components/SoundBox/ColumnOfButtons/getButton.js
+++ src/components/SoundBox/ColumnOfButtons/getButton.js
@@ -4,17 +4,17 @@ import { ButtonStyles, getBlinker } from '../../../styles/ButtonStyles';
 const getButton = color => {
   const blinker = getBlinker(color);
   return styled.button`
-    ${ButtonStyles} ${props =>
-        props.selected &&
-        !props.isLoading &&
-        css`
-          background: ${color};
-        `} ${props =>
-        props.isLoading &&
-        props.selected &&
-        css`
-          animation: ${blinker} 1s steps(2, end) infinite;
-        `};
+    ${ButtonStyles} ${props => {
+        if (props.selected) {
+          return props.isLoading
+            ? css`
+                animation: ${blinker} 1s steps(2, end) infinite;
+              `
+            : css`
+                background: ${color};
+              `;
+        }
+      }};
   `;
 };
 
